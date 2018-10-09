344b3e1af5fe13531faba8578a0414a9479d0af2
jyhs <jyhs_123@163.com>
2018-10-09 21:17:36 +0800

    update

--- src/common/config/config.production.js
+++ src/common/config/config.production.js
@@ -5,5 +5,12 @@
 //   cert: fs.readFileSync('/usr/local/nginx/ssl/1_huanjiaohu.com_bundle.crt')
 // };
 module.exports = {
-  workers: 0
+  workers: 0,
+  image: {
+    user: '/usr/local/nginx/html/image/user',
+    ad: '/usr/local/nginx/html/image/ad',
+    material: '/usr/local/nginx/html/image/material',
+    notice: '/usr/local/nginx/html/image/notice',
+    bill: '/usr/local/nginx/html/image/bill'
+  }
 };
